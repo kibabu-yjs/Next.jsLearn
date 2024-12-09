c465cd36ae06ef6874cc3a43e618005b2217cf06
kjshan89 <amuhammadben@gmail.com>
2024-12-09 13:27:44 +0300

    modified:   app/dashboard/(overview)/page.tsx 	modified:   app/ui/dashboard/latest-invoices.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,8 +8,13 @@ import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
 
-  const latestInvoices = await fetchLatestInvoices();
-
+  // const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
   const cardDatas = await fetchCardData()
   return (
     <main>
@@ -17,12 +22,12 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={cardDatas.totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={cardDatas.totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={cardDatas.numberOfInvoices} type="invoices" />
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
-          value={cardDatas.numberOfCustomers}
+          value={numberOfCustomers}
           type="customers"
         />
       </div>
@@ -30,7 +35,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoices/>} >
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );

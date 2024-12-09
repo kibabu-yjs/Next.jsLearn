62590c868fdbaef258f49c459e2fc31e096f712c
kjshan89 <amuhammadben@gmail.com>
2024-12-09 10:59:47 +0300

    /query route.ts modified

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,29 +1,26 @@
 import { db } from "@vercel/postgres";
 
+const client = await db.connect();
 
-// async function listInvoices() {
-  // 	const data = await client.sql`
-  //     SELECT invoices.amount, customers.name
-  //     FROM invoices
-  //     JOIN customers ON invoices.customer_id = customers.id
-  //     WHERE invoices.amount = 666;
-  //   `;
-  
-  // 	return data.rows;
-  // }
-  
-  export async function GET() {
-    // return Response.json({
-      //   message:
-      //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
-      // });
-      
-    try {
-      const client = await db.connect().then((res)=>console.log(res)).catch(err=>console.error('error found here::', err))
+async function listInvoices() {
+	const data = await client.sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
 
-      // const data = await client.sql`SELECT 1;`
-  	  return Response.json({"hello": "world"});
-    } catch (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+	return data.rows;
+}
+
+export async function GET() {
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
